7405c296fafffb550872ff7f1c27608dc3965c49
Priyank Solanki <psolanki@condorcet93.fr>
2024-11-04 16:12:10 +0100

    remove bugs

--- affichage_prim.py
+++ affichage_prim.py
@@ -6,18 +6,17 @@ import prim
 #listes
 sommets = [] 
 arretes = []
-sommets_visites = []
 arbre_couvrant = []
 #remplissage des listes
 main.parse_metro(sommets, arretes)
-prim.prim_algo(sommets, arretes, arbre_couvrant, sommets_visites)
+prim.prim_algo(sommets, arretes, arbre_couvrant)
 # Créer un graphe vide
 graphe = nx.Graph()
 
 
-def creation_graph(sommets_visites, arbre_couvrant, graphe) :
+def creation_graph(arbre_couvrant, graphe) :
     for s in sommets :
-        graphe.add_node(s['numSommet'], group=""+str(s['numLigne']))
+        graphe.add_node(s['numSommet'], group=s['numLigne'])
     for a in arbre_couvrant :
         graphe.add_edge(a['S1'], a['S2'])
 
@@ -43,14 +42,16 @@ def afficher_graphe(graphe) :
         '14' : '#662483'
     }
     colors = [color_map[graphe.nodes[node]['group']] for node in graphe.nodes]
+    labels = {node: graphe.nodes[node]['group'] for node in graphe.nodes}
+    pos = nx.kamada_kawai_layout(graphe)
+
     # Tracer le graphe
     plt.figure(figsize=(10, 10))
-    pos = nx.kamada_kawai_layout(graphe)
-    nx.draw(graphe, pos, with_labels=True, node_color=colors, edge_color="black", node_size=100, font_size=5)
+    nx.draw(graphe, pos, with_labels=True,  node_color=colors, edge_color="black", node_size=100, font_size=7)
 
     # Afficher le graphe
     plt.title("Affichage d'un graphe avec NetworkX")
     plt.show()
 
-creation_graph(sommets_visites, arbre_couvrant, graphe)
+creation_graph(arbre_couvrant, graphe)
 afficher_graphe(graphe)
--- prim.py
+++ prim.py
@@ -1,25 +1,11 @@
-import main
-
-#listes
-sommets = [] 
-arretes = []
-sommets_visites = []
-arbre_couvrant = []
-#remplissage des listes
-main.parse_metro(sommets, arretes)
-
 # Algorithme de Prim
-def prim_algo(sommets, arretes, arbre_couvrant, sommets_visites) :
-
-    nouveaux_sommets_decouverts = [] # les nouveaux sommets decouverts a chaque iteration
+def prim_algo(sommets, arretes, arbre_couvrant) :
+    sommets_visites = []
     arretes_disponible = [] # les arretes disponlible
-
-    nouveaux_sommets_decouverts.append(sommets[0]['numSommet']) # partir du premier sommet de la liste
-
-    while (len(sommets_visites) != len(sommets)) :
-
-        remplissage_arretes_dispo(nouveaux_sommets_decouverts, arretes_disponible, arretes, sommets_visites) # avoir les arretes disponible selon les nouveaux sommets decouvert
-        verif_arretes_dispo(sommets_visites, arretes_disponible) # verifie si les arretes disponible qont toujours disponible
+    sommets_visites.append(sommets[0]['numSommet']) # partir du premier sommet de la liste
+    a=0
+    while (len(sommets) > len(sommets_visites)) :
+        remplissage_arretes_dispo(sommets_visites[len(sommets_visites)-1], arretes_disponible, arretes, sommets_visites) # avoir les arretes disponible selon les nouveaux sommets decouvert
         arrete_minimum = minimum(arretes_disponible) # prendre l'indice le l'arrete la plus petite
         
         if(arrete_minimum == -1) :
@@ -28,10 +14,13 @@ def prim_algo(sommets, arretes, arbre_couvrant, sommets_visites) :
 
         arbre_couvrant.append(arrete_minimum) # ajouter l'arrete la plus petite à l'arbre couvrant
         if(arrete_minimum['S1'] in sommets_visites) :
-            nouveaux_sommets_decouverts.append(arrete_minimum['S2'])
-        else :
-            nouveaux_sommets_decouverts.append(arrete_minimum['S1'])
+            sommets_visites.append(arrete_minimum['S2'])
+        elif(arrete_minimum['S2'] in sommets_visites) :
+            sommets_visites.append(arrete_minimum['S1'])
         arretes_disponible.remove(arrete_minimum)
+        verif_arretes_dispo(sommets_visites, arretes_disponible, arretes) # verifie si les arretes disponible sont toujours disponible
+        a=a+1
+
     print("nombre de sommets visites : ",len(sommets_visites)) # 376 sommmets en tout
     print("nombre d'arretes de l'ACPM : ",len(arbre_couvrant))
     print("nombre de doublons de sommets visites: ",len(sommets_visites) - len(set(sommets_visites)))
@@ -46,27 +35,28 @@ def minimum(arretes_dispo) :
     a_min = arretes_dispo[0]['poid']
     indice = 0
 
-    for i in range (1,len(arretes_dispo) ) :
-        if(arretes_dispo[i]['poid']<a_min) :
+    for i in range (1,len(arretes_dispo)) :
+        if(arretes_dispo[i]['poid'] < a_min) :
             a_min = arretes_dispo[i]['poid']
             indice = i
     return arretes_dispo[indice]
 
 
 #fonction qui donne les arretes disponible selon les nouveaux sommets decouverts
-def remplissage_arretes_dispo(nouveaux_sommets_decouverts, arretes_disponible, arretes, sommets_visites) :
-    for s in nouveaux_sommets_decouverts :
-        for a in arretes :
-            if(a not in arretes_disponible) :
-                if(a['S1'] == s and a['S2'] not in sommets_visites) or (a['S2'] == s and a['S1'] not in sommets_visites) :
-                    arretes_disponible.append(a)
-                    arretes.remove(a)
-        sommets_visites.append(s)
-        nouveaux_sommets_decouverts.remove(s)
+def remplissage_arretes_dispo(nouveau_sommet_decouvert, arretes_disponible, arretes, sommets_visites) :
+    for a in arretes :
+        if a not in arretes_disponible :
+            if ((a['S1'] == nouveau_sommet_decouvert) and (a['S2'] not in sommets_visites)) :
+                arretes_disponible.append(a)
+            if ((a['S2'] == nouveau_sommet_decouvert) and (a['S1'] not in sommets_visites)) :
+                arretes_disponible.append(a)
 
 # fonction qui permet de verifier si les 2 sommmets d'une arrete disponible est deja visites
-def verif_arretes_dispo(sommets_visites, arretes_dispo) :
+def verif_arretes_dispo(sommets_visites, arretes_dispo, arretes) :
     for a in arretes_dispo :
-        if (a['S1'] in sommets_visites and a['S2'] in sommets_visites) :
+        if (a['S1'] in sommets_visites) and (a['S2'] in sommets_visites) :
             arretes_dispo.remove(a)
+    for a in arretes :
+        if (a['S1'] in sommets_visites) and (a['S2'] in sommets_visites) :
+            arretes.remove(a)
 
